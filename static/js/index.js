7429860ad4ff8c5b8fbcaeca3fb57b0fe363bfa1
Josh Goldberg <diogenesthecynic@hotmail.com>
2015-05-10 17:10:39 -0400

    Added JS for email tags

---
+++ static/js/index.js
@@ -0,0 +1,17 @@
+(function () {
+    var elements = document.querySelectorAll(".email"),
+        element, email, i;
+
+    for (i = 0; i < elements.length; i += 1) {
+        element = elements[i];
+
+        if (element.className.indexOf("spaced") !== -1) {
+            email = element.textContent.replace(" at ", " @ ");
+        } else {
+            email = element.textContent.replace(" at ", "@");
+        }
+
+        element.textContent = email;
+        element.setAttribute("href", "mailto:" + email);
+    }
+})();
